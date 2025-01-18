8fdc2d2078352c0576b1975a26df1e9f43ff578a
PythonToGo <pythontogoplease@gmail.com>
2025-01-18 23:04:28 +0100

    language dap ui conflict

--- nvim/lua/plugins/neo-tree.lua
+++ nvim/lua/plugins/neo-tree.lua
@@ -8,3 +8,29 @@ return {
     -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
   }
 }
+--return {
+--  "nvim-neo-tree/neo-tree.nvim",
+--  branch = "v3.x", -- 안정적인 최신 브랜치 사용
+--  requires = {
+--    "nvim-lua/plenary.nvim",
+--    "nvim-tree/nvim-web-devicons", -- 아이콘 지원
+--    "MunifTanjim/nui.nvim", -- UI 구성 요소
+--  },
+--  config = function()
+--    require("neo-tree").setup({
+--      filesystem = {
+--        filtered_items = {
+--          hide_dotfiles = false, -- 숨김 파일 표시
+--          hide_gitignored = true,
+--        },
+--        follow_current_file = true,
+--        hijack_netrw_behavior = "open_current", -- Netrw 비활성화
+--        use_libuv_file_watcher = true,
+--      },
+--      window_picker = {
+--        enable = true,
+--        picker = require("window-picker").pick_window,
+--      },
+--    })
+--  end,
+--}
--- nvim/lua/plugins/nvim-cmp.lua
+++ nvim/lua/plugins/nvim-cmp.lua
@@ -1,51 +1,83 @@
 return {
-    {
-        "hrsh7th/nvim-cmp",
-        event = { "BufReadPost", "BufNewFile" },
-        dependencies = {
-            {
-                "L3MON4D3/LuaSnip",
-                version = "v2.*",
-                build = "make install_jsregexp",
-            },
-            "saadparwaiz1/cmp_luasnip",
-            "hrsh7th/cmp-nvim-lsp",
-            "hrsh7th/cmp-buffer",
-            "hrsh7th/cmp-path",
-            "rafamadriz/friendly-snippets",
-        },
-        config = function()
-            local cmp = require("cmp")
-            local luasnip = require("luasnip")
-
-            -- load snippets
-            require("luasnip.loaders.from_vscode").lazy_load()
-
-            cmp.setup({
-                snippet = {
-                    expand = function(args)
-                        luasnip.lsp_expand(args.body)
-                    end,
-                },
-                window = {
-                    completion = cmp.config.window.bordered(),
-                    documentation = cmp.config.window.bordered(),
-                },
-                mapping = cmp.mapping.preset.insert({
-                    ["<C-b>"] = cmp.mapping.scroll_docs(-4),
-                    ["<C-f>"] = cmp.mapping.scroll_docs(4),
-                    ["<C-Space>"] = cmp.mapping.complete(),
-                    ["<C-e>"] = cmp.mapping.abort(),
-                    ["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-                }),
-                -- autocompletion sources
-                sources = cmp.config.sources({
-                    { name = "nvim_lsp" },                    -- lsp
-                    { name = "buffer",  max_item_count = 5 }, -- text within current buffer
-                    { name = "path",    max_item_count = 3 }, -- file system paths
-                    { name = "luasnip", max_item_count = 3 }, -- snippets
-                }),
-            })
+  "hrsh7th/nvim-cmp",
+  dependencies = {
+    "hrsh7th/cmp-nvim-lsp",
+    "hrsh7th/cmp-buffer",
+    "hrsh7th/cmp-path",
+    "hrsh7th/cmp-cmdline",
+    "L3MON4D3/LuaSnip",         -- Snippet engine
+    "saadparwaiz1/cmp_luasnip", -- Snippet source
+  },
+  config = function()
+    local cmp = require("cmp")
+    cmp.setup({
+      snippet = {
+        expand = function(args)
+          require("luasnip").lsp_expand(args.body)
         end,
-    },
+      },
+      mapping = cmp.mapping.preset.insert({
+        ["<C-n>"] = cmp.mapping.select_next_item(),
+        ["<C-p>"] = cmp.mapping.select_prev_item(),
+        ["<CR>"] = cmp.mapping.confirm({ select = true }),
+      }),
+      sources = cmp.config.sources({
+        { name = "nvim_lsp" },
+        { name = "buffer" },
+        { name = "path" },
+      }),
+    })
+  end,
 }
+
+--return {
+--    {
+--        "hrsh7th/nvim-cmp",
+--        event = { "BufReadPost", "BufNewFile" },
+--        dependencies = {
+--            {
+--                "L3MON4D3/LuaSnip",
+--                version = "v2.*",
+--                build = "make install_jsregexp",
+--            },
+--            "saadparwaiz1/cmp_luasnip",
+--            "hrsh7th/cmp-nvim-lsp",
+--            "hrsh7th/cmp-buffer",
+--            "hrsh7th/cmp-path",
+--            "rafamadriz/friendly-snippets",
+--        },
+--        config = function()
+--            local cmp = require("cmp")
+--            local luasnip = require("luasnip")
+--
+--            -- load snippets
+--            require("luasnip.loaders.from_vscode").lazy_load()
+--
+--            cmp.setup({
+--                snippet = {
+--                    expand = function(args)
+--                        luasnip.lsp_expand(args.body)
+--                    end,
+--                },
+--                window = {
+--                    completion = cmp.config.window.bordered(),
+--                    documentation = cmp.config.window.bordered(),
+--                },
+--                mapping = cmp.mapping.preset.insert({
+--                    ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+--                    ["<C-f>"] = cmp.mapping.scroll_docs(4),
+--                    ["<C-Space>"] = cmp.mapping.complete(),
+--                    ["<C-e>"] = cmp.mapping.abort(),
+--                    ["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+--                }),
+--                -- autocompletion sources
+--                sources = cmp.config.sources({
+--                    { name = "nvim_lsp" },                    -- lsp
+--                    { name = "buffer",  max_item_count = 5 }, -- text within current buffer
+--                    { name = "path",    max_item_count = 3 }, -- file system paths
+--                    { name = "luasnip", max_item_count = 3 }, -- snippets
+--                }),
+--            })
+--        end,
+--    },
+--}
